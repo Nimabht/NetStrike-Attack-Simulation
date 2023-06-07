49fd7e1294b873761b943eb3b4c962a49861afe2
nimabht <nimabeheshtaein99@gmail.com>
2023-06-07 12:24:44 +0330

    [refactor] class validation added to app

--- attacker/src/target/target.service.ts
+++ attacker/src/target/target.service.ts
@@ -1,21 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { CreateTargetDto } from './dto/create-target.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Target } from './schemas/target.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class TargetService {
+  constructor(@InjectModel(Target.name) private catModel: Model<Target>) {}
+
   create(createTargetDto: CreateTargetDto) {
     return 'This action adds a new target';
   }
 
   findAll() {
     return `This action returns all target`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} target`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} target`;
   }
 }

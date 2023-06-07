b678c896154af57015afed62af699537c1b0b536
nimabht <nimabeheshtaein99@gmail.com>
2023-06-07 12:53:33 +0330

    [feat] axios added to app

--- attacker/src/target/target.module.ts
+++ attacker/src/target/target.module.ts
@@ -3,9 +3,12 @@ import { TargetService } from './target.service';
 import { TargetController } from './target.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Target, TargetSchema } from './schemas/target.schema';
+import { HttpModule } from '@nestjs/axios';
+
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Target.name, schema: TargetSchema }]),
+    HttpModule,
   ],
   controllers: [TargetController],
   providers: [TargetService],

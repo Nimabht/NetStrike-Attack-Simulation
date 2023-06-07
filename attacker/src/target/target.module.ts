c19682df71f63a57fd0393a3e6871e31618b4162
nimabht <nimabeheshtaein99@gmail.com>
2023-06-07 12:21:18 +0330

    [refactor] schema connected to its own module

--- attacker/src/target/target.module.ts
+++ attacker/src/target/target.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TargetService } from './target.service';
 import { TargetController } from './target.controller';
-
+import { MongooseModule } from '@nestjs/mongoose';
+import { Target, TargetSchema } from './schemas/target.schema';
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Target.name, schema: TargetSchema }]),
+  ],
   controllers: [TargetController],
-  providers: [TargetService]
+  providers: [TargetService],
 })
 export class TargetModule {}
